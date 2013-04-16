061ebbf747dc00084fb1cf6b6aa5f794573ec082
Michael Haas <haas@cl.uni-heidelberg.de>
2013-04-16 19:52:35 +0200

    Add TweetIDExtractor
    Also add getFieldType for ARFF meta data

--- src/featureextraction/baseextractor.py
+++ src/featureextraction/baseextractor.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
 
 class BaseExtractor(object):
 
@@ -13,6 +14,20 @@ class BaseExtractor(object):
     def getFields(self):
         raise NotImplementedError
    
+    # ARFF datatype declaration
+    def getFieldType(self, field):
+        return "real"
+
+
+class TweetIDExtractor(BaseExtractor):
+    def __init__(self):
+        pass
+
+    def extractFeatures(self, tweet):
+        return tweet.tweetID
+
+    def getFields(self):
+        return ["tweetID"]
 
     
 
