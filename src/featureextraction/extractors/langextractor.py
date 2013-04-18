9de281e932d9bf6832bd58f7b0764a14190c3207
Michael Haas <haas@cl.uni-heidelberg.de>
2013-04-18 13:29:15 +0200

    Add lang feature extractor

---
+++ src/featureextraction/extractors/langextractor.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from baseextractor import BaseExtractor
+from collections import OrderedDict
+#{u'pos_smileys': 0.0, u'pos_tokens': 0.0, u'tokens': 22.0, u'overall_sentiment_score': 0.0, u'neutral_tokens': 22.0, u'neg_tokens': 0.0, u'neutral_smileys': 0.0, u'neg_smileys': 0.0, u'normalized_sentiment_score': 0.0}
+import ldig.server
+
+
+class LDIGLangExtractor(BaseExtractor):
+
+
+    def __init__(self, model):
+       self.d = ldig.server.Detector(model)
+       self.field = "lang_ldig"
+
+    def extractFeatures(self, tweet):
+        ret = OrderedDict()
+        stats = self.d.detect(tweet.tweet)
+        # get best match
+        assert len(stats["labels"]) == len(stats["prob"])
+        bestMatch = None
+        bestMatchProp = -1
+        for index in xrange(len(stats["labels"])):
+            cur = stats["prob"][index]
+            if cur > bestMatchProp:
+                bestMatchProp = cur
+                bestMatch = stats["labels"][index]
+        ret[self.field] = bestMatch
+        return ret 
+
+    def getFields(self):
+        return [self.field]
+    
+    def getFieldType(self, field):
+        return "{" + ",".join(self.d.labels) + "}"
+        
+
+
+if __name__ == "__main__":
+    import sys
+    from tweetloaders.goldtweetloader import GoldTweet
+    if len(sys.argv) < 2:
+        print >> sys.stderr, "Usage: script.py modelDir"
+        print >> sys.stderr, "Provide LDIG model directory as first argument."
+        sys.exit(1)
+    d = LDIGLangExtractor(sys.argv[1])
+    t = GoldTweet("fakeID")
+    t.tweet = "Dies ist ein deutscher Tweet"
+    print d.extractFeatures(t)
+    print d.getFieldType("blah")
